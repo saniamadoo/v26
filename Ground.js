3e598cacab250bce420d8af3a177438b6ce3f427
Sania Madoo <saniamadoo@Sanias-MacBook-Air.local>
2021-01-07 19:31:46 +0530

    hii this is my first commit

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill(66,42,24);
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
